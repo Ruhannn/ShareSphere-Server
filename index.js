1abd9f6960e6762fe3c4ea906966b082a3525296
Ruhannn <kamiayaka09@gmail.com>
2023-11-09 19:09:51 +0600

    booking-service done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,19 +18,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("ShareSphere").collection("services");
+    const bookingCollection = client.db("ShareSphere").collection("booking");
+
     app.post("/add-service", async (req, res) => {
       const formData = req.body;
       const result = await serviceCollection.insertOne(formData);
       res.send(result);
     });
+
     app.get("/services", async (req, res) => {
       const result = await serviceCollection.find().toArray();
       res.send(result);
     });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/booking-service", async (req, res) => {
+      const bookingData = req.body;
+      const result = await bookingCollection.insertOne(bookingData);
+      res.send(result);
+    });
     console.log("You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
